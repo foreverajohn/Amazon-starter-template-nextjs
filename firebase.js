ea16e54885a5c134c5d2f4bf4ff4e5de2a425237
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-08-03 18:22:10 -0500

    authentication

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtqXZo7bxdrcsnD2zYX-rrzqr0pTsuPHI",
+    authDomain: "clone-f396b.firebaseapp.com",
+    projectId: "clone-f396b",
+    storageBucket: "clone-f396b.appspot.com",
+    messagingSenderId: "297345261153",
+    appId: "1:297345261153:web:b6488c95272731d6195420"
+  };
