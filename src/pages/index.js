4880b17ed2e0286133bae593a67ceaffe4ff6ac4
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-08-03 16:41:23 -0500

    banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,20 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-gray-100'>
       <Head>
         <title>Amazon Clone</title>
       </Head>
 
       <Header />
+      <main className='max-w-screen-2xl mx-auto'>
+        <Banner />
 
-      {/* Banner */}
-
-      {/* Product Feed */}
-
+        {/* Product Feed */}
+      </main>
     </div>
   );
 }
